493675b8aad55e9f890b044f4383339b49326ae9
DraSilvana <smfloresb@coreo.itlalaguna.edu.mx>
2026-06-29 14:21:18 -0600

    compplemento de codigo

--- ordenamiento.cpp
+++ ordenamiento.cpp
@@ -62,9 +62,25 @@ int main() {
  
         switch (opcion) {
             case 1:
-                cout << ">>> Burbuja (Bubble Sort)" << endl;
+                
+            cout << ">>> Burbuja (Bubble Sofor (int i=0; i<TAM -1; I++rt)" << endl;
                 cout << "    Antes : "; 
-      
+                mostrarArreglo(original,TAM);
+                for (int i=0; i<TAM;i++)
+                  temp[i]=original[i];
+
+                 for (int i=0; i<TAM-1; i++)
+                   if (temp[i] > temp[j])
+                   {
+                     int aux= temp[i];
+                     temp[i]= temp[j];
+                     temp[j]=aux;           
+
+
+
+
+                   } 
+                                       
                 cout << "    Despues: "; 
                 break;
  
