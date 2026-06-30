a2c63a83ec0e8616e397da4fed73b0b6a5170f2f
Jlzapata60 <jlzapatadavila@gmail.com>
2026-06-29 18:38:57 -0600

    Implementación de las funciones de ordenamiento

--- ordenamiento.cpp
+++ ordenamiento.cpp
@@ -32,7 +32,7 @@ void heapify(int arr[], int n, int i);          // <-- completar
 //  ARREGLO ORIGINAL (fijo, no se modifica)
 // ============================================================
 int original[TAM] = {45, 12, 78, 3, 56, 89, 23, 67, 34, 9};
- 
+int ordenado[TAM]; 
 // ============================================================
 //  MAIN
 // ============================================================
@@ -55,31 +55,44 @@ int main() {
         cout << "----------------------------------------" << endl;
         cout << " Elige una opcion: ";
         cin  >> opcion;
- 
-    
- 
+
         cout << endl;
- 
+    
         switch (opcion) {
             case 1:
                 cout << ">>> Burbuja (Bubble Sort)" << endl;
                 cout << "    Antes : "; 
-      
-                cout << "    Despues: "; 
+                mostrarArreglo(original, TAM);
+                cout << "    Despues: ";
+                for (int i = 0; i < TAM; i++){
+                    ordenado[i] = original[i];
+                };
+                burbuja(ordenado,TAM);
+                mostrarArreglo(ordenado, TAM); 
                 break;
- 
+
             case 2:
                 cout << ">>> QuickSort" << endl;
                 cout << "    Antes : ";  
-                
-                cout << "    Despues: "; 
+                mostrarArreglo(original, TAM);                
+                cout << "    Despues: ";
+                for (int i = 0; i < TAM; i++){
+                    ordenado[i] = original[i];
+                };
+                quickSort(ordenado, 0, TAM-1);
+                mostrarArreglo(ordenado, TAM);  
                 break;
  
             case 3:
                 cout << ">>> HeapSort" << endl;
                 cout << "    Antes : ";  
-               
+                mostrarArreglo(original, TAM);               
                 cout << "    Despues: "; 
+                for (int i = 0; i < TAM; i++){
+                    ordenado[i] = original[i];
+                };
+                heapSort(ordenado, TAM);
+                mostrarArreglo(ordenado, TAM);  
                 break;
  
             case 0:
@@ -103,4 +116,76 @@ void mostrarArreglo(int arr[], int n) {
         if (i < n - 1) cout << ",";
     }
     cout << " ]" << endl;
+}
+
+// Ordenamiento burbuja
+void burbuja(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        // Banderas para optimizar: si no hay intercambios, el arreglo ya está ordenado
+        bool Intercambio = false; 
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Intercambio de elementos
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                Intercambio = true;
+            }
+        }
+        
+        if (!Intercambio) {
+            break;
+        }
+    }    
+}
+
+// Ordenamiento quickSort
+int particionar(int arr[], int inicio, int fin) {
+    int pivote = arr[fin];
+    int i = (inicio - 1);
+
+    for (int j = inicio; j <= fin - 1; j++) {
+        if (arr[j] <= pivote) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[fin]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int izq, int der){
+    if (izq < der) {
+        int pi = particionar(arr, izq, der);
+        quickSort(arr, izq, pi - 1);
+        quickSort(arr, pi + 1, der);
+    }
+}
+
+// Ordenamiento heapSort
+void mantenerHeap(int arr[], int n, int i) {
+    int mayor = i;
+    int izq = 2 * i + 1;
+    int der = 2 * i + 2;
+
+    if (izq < n && arr[izq] > arr[mayor])
+        mayor = izq;
+
+    if (der < n && arr[der] > arr[mayor])
+        mayor = der;
+
+    if (mayor != i) {
+        swap(arr[i], arr[mayor]);
+        mantenerHeap(arr, n, mayor);
+    }
+}
+
+void heapSort(int arr[], int n){
+    for (int i = n / 2 - 1; i >= 0; i--)
+        mantenerHeap(arr, n, i);
+
+    for (int i = n - 1; i > 0; i--) {
+        swap(arr[0], arr[i]);
+        mantenerHeap(arr, i, 0);
+    }
 }
